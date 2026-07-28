74c2dc4fc8e0734ed649f83806e8452157f22d34
Nishanth-tech-45 <nisha5471na@gmail.com>
2026-07-28 13:24:11 +0530

    Add files via upload

---
+++ if only.py
@@ -0,0 +1,106 @@
+'''
+a=10
+if a%2==0:
+   print("even number")
+if a%2==1:
+   print("odd number")
+'''
+
+'''
+a = 10
+b = 22
+if a>b:
+    print("a is grater")
+if b>a:
+   print("b is grater")
+'''
+
+'''
+a = 10
+b = 8
+if a<b:
+    print("a is smaller")
+if b<a:
+    print("b is smaller")
+
+'''
+
+
+'''
+age=int(input("enter your age: "))
+if age<18:
+    print(" not eligible for vote")
+if age>=18:
+    print("eligible for vote")
+'''
+
+
+'''
+a=int(input("enter a value:"))
+
+if a>0:
+    print("positive")
+
+if a<0:
+    print("negative")
+
+if a==0:
+    print("netural")
+'''
+
+'''
+b=int(input("enter buy value:"))
+s=int(input("enter sell value:"))
+
+if s>b:
+    print("profit",s-b)
+if s<b:
+    print("loss",s-b)
+'''
+
+'''
+a=int(input("angle a:"))
+b=int(input("angle b:"))
+c=int(input("angle c:"))
+
+t=a+b+c
+
+if t==180:
+    print("its triangle")
+if t!=180:
+    print("its not triangle")
+'''
+
+
+'''
+p=-78
+
+if 0<p :
+    print("positive",p)
+if 0>p:
+    p*=-1
+    print("positive abs",p)
+
+'''
+
+
+
+
+
+
+
+    
+
+
+
+
+ 
+
+
+
+    
+
+
+
+
+
